f1c222fa7197cc524b517b8cd0781856c7b6c7b3
Gage <gw_eveland@hotmail.com>
2022-04-19 10:43:44 -0500

    Add project files.

---
+++ Controllers/CheeseController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+
+        static private List<string> Cheeses = new List<string>();
+        public IActionResult Index()
+        {
+  
+
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name)
+        {
+            //Add new cheese to existing cheeses
+            Cheeses.Add(name);
+
+            return Redirect("/Cheese");
+
+        } 
+
+
+        
+    }
+}
